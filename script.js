70b0ea7adf9bd1efcabb313f104231bba5bccd77
Abiel101 <abielortega90@gmail.com>
2023-04-05 23:35:01 -0500

    figured out how to open and close thing using javascript which was the main goal. Know moving on to adding cool animations to open and closing part of the dropdown. I solved it by not giving up on a possible solution and for looking for different ways i could come up with my own solution

---
+++ script.js
@@ -0,0 +1,51 @@
+const body = document.getElementById("body");
+const featuresBtn = document.getElementById("features");
+const companyBtn = document.getElementById("company");
+const featureDropdown = document.getElementById("feature_dropdown");
+const companyDropdown = document.getElementById("company_dropdown");
+
+// Selecting the arrows
+const featureArrowDown = document.getElementById("feature-arrow-down");
+const featureArrowUp = document.getElementById("feature-arrow-up");
+const companyArrowDown = document.getElementById("company-arrow-down");
+const companyArrowUp = document.getElementById("company-arrow-up");
+
+let triggerClick;
+
+// Reusable functions
+function openDropDown(dropdownLink, arrowDown, arrowUp){
+    dropdownLink.classList.remove("display-none");
+    arrowDown.classList.add("display-none");
+    arrowUp.classList.remove("display-none");
+    triggerClick = true;
+    console.log(triggerClick);
+};
+function closeDropDown(dropdownLink, arrowDown, arrowUp){
+    dropdownLink.classList.add("display-none");
+    arrowDown.classList.remove("display-none");
+    arrowUp.classList.add("display-none");
+    triggerClick = false;
+    console.log(triggerClick);
+};
+
+
+// Featured open and close
+featuresBtn.onmouseover = function(){
+    openDropDown(featureDropdown, featureArrowDown, featureArrowUp);
+    // console.log("opened");
+}
+featureDropdown.onmouseleave = function(){
+    closeDropDown(featureDropdown, featureArrowDown, featureArrowUp);
+    // console.log("close");
+}
+// Company open and close
+companyBtn.onmouseover = function(){
+    openDropDown(companyDropdown, companyArrowDown, companyArrowUp);
+    // console.log("opened");
+}
+companyDropdown.onmouseleave = function(){
+    closeDropDown(companyDropdown, companyArrowDown, companyArrowUp);
+    // console.log("close");
+}
+
+
